a685ceadd9bc6a33b6f8890c9511aa856a5ca18c
EricVanDeGriesel <55184120+EricVanDeGriesel@users.noreply.github.com>
2020-08-05 23:16:14 +0200

    Erweiterung Bestellvorgang

--- js/bestellUebersicht.js
+++ js/bestellUebersicht.js
@@ -4,7 +4,7 @@ function submitToView() {
     var i;
         for (i = 0; i < x.length ;i++) {
             if (x.elements[i].type === "text" ){
-                text += x.elements[i].value + "<br>";
+                text += x.elements[i].value + "&";
                 localStorage.setItem(x.elements[i].id , x.elements[i].value);
             }   
         }
@@ -31,65 +31,34 @@ function showElement(myElementId){
 }
 
 
-function showData(){
-   /* for (var i = 0; i < localStorage.length; i++){
-        $('#userOutput').append(localStorage.getItem(localStorage.key(i)));
-    } */
-    createAccountDiv();
 
+function showData(){
+   
+    createAccountDiv();
+    createInvoiceDiv();
 }
 
 function createAccountDiv(){
-
-    var accArray = new Array();
-
-    for (var i = 0; i < localStorage.length; i++){
-        var store = localStorage.key(i);
-        if (store.startsWith("acc")){
-            accArray.push(store);
-        } 
-    }
-    console.log(accArray);
-
-        var div = document.getElementById('userOutput');
-        var tbl = document.createElement('table');
-        tbl.style.width = '100%';
-        tbl.setAttribute('border', '1');
-        var tbdy = document.createElement('tbody');
-
-        var tr = document.createElement('tr');
-        var td = document.createElement('td');
-
-        td.appendChild(document.createTextNode(localStorage.getItem('vorname')));
-        tr.appendChild(td);
-        td.appendChild(document.createTextNode(localStorage.getItem('nachname')));
-        tr.appendChild(td);
-        tbl.appendChild(tr);
-
-
-
-        /* for (var i = 0; i < 3; i++) {
-          var tr = document.createElement('tr');
-          for (var j = 0; j < 2; j++) {
-            if (i == 2 && j == 1) {
-              break
-            } else {
-              var td = document.createElement('td');
-              td.appendChild(document.createTextNode('\u0020'))
-              tr.appendChild(td)
-            }
-          }
-          tbdy.appendChild(tr);
-        } */
-        tbl.appendChild(tbdy);
-        div.appendChild(tbl)
+        
+       $("#accountDaten").html(
+        "<h3>Accountdaten</h3>"+   
+        localStorage.getItem('accVorname')+","+localStorage.getItem('accNachname')+"</br>"+
+        localStorage.getItem('accStraße')+" "+"</br>"+
+        localStorage.getItem('accPlz')+" "+localStorage.getItem('accOrt')
+       );
       
-}
+}    
+
 
 function createInvoiceDiv(){
-
+    $("#invoiceDaten").html(
+        "<h3>Rechnungsdaten</h3>"+   
+        localStorage.getItem('invoiceVorname')+","+localStorage.getItem('invoiceNachname')+"</br>"+
+        localStorage.getItem('invoiceStraße')+" "+"</br>"+
+        localStorage.getItem('invoicePlz')+" "+localStorage.getItem('invoiceOrt')
+       );
 }
 
-function createarticleDiv(){
+function createArticleDiv(){
 
 }
