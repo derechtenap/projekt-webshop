ac460efe947c144c5199c6e0f4effb537605b071
AntonyPaok <59829071+AntonyPaok@users.noreply.github.com>
2020-08-08 19:16:21 +0200

    Neue Datensätze,. Verbesserung DB und PHP Variable

--- webshopdb.sql
+++ webshopdb.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Erstellungszeit: 03. Aug 2020 um 21:31
+-- Erstellungszeit: 08. Aug 2020 um 19:00
 -- Server-Version: 10.4.13-MariaDB
 -- PHP-Version: 7.4.8
 
@@ -68,16 +68,18 @@ CREATE TABLE `t_plz` (
 
 CREATE TABLE `t_produkte` (
   `Prod_ID` int(11) NOT NULL,
-  `ProduktName` varchar(20) NOT NULL,
+  `Produkt_Name` varchar(20) NOT NULL,
+  `Anzeige_Name` varchar(30) NOT NULL,
+  `Kategorie` varchar(10) NOT NULL,
   `Nettopreis` float NOT NULL,
-  `Lagerbesstand` int(11) NOT NULL,
+  `Lagerbestand` int(11) NOT NULL,
   `Sales` tinyint(1) NOT NULL,
   `Beschreibung` text DEFAULT NULL,
-  `kcal` int(11) DEFAULT NULL,
+  `Kalorien` int(11) DEFAULT NULL,
   `Fett` float DEFAULT NULL,
   `Kohlenhydrate` float DEFAULT NULL,
-  `davonZucker` float DEFAULT NULL,
-  `Eiweiß` float DEFAULT NULL,
+  `Zucker` float DEFAULT NULL,
+  `Protein` float DEFAULT NULL,
   `Herkunft` varchar(3) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
@@ -85,14 +87,21 @@ CREATE TABLE `t_produkte` (
 -- Daten für Tabelle `t_produkte`
 --
 
-INSERT INTO `t_produkte` (`Prod_ID`, `ProduktName`, `Nettopreis`, `Lagerbesstand`, `Sales`, `Beschreibung`, `kcal`, `Fett`, `Kohlenhydrate`, `davonZucker`, `Eiweiß`, `Herkunft`) VALUES
-(1, 'Pute-Ei-Sandwich', 1.21, 10, 0, 'Vollkorntoast belegt mit Pute, Ei, Paprika und Majonaise', 246, 14.6, 17.2, 2.9, 10.1, 'DEU'),
-(2, 'Kartoffelsalat-Joghu', 1.87, 5, 1, 'Kartoffelsalat mit Joghurtcreme und Schnittlauch', 126, 6.4, 15.7, 4.3, 1.4, 'DEU'),
-(3, 'Banane', 0.45, 25, 0, 'Bananen werden pro Stück verkauft. \r\nDie Nährwerte beziehen sich aber auf 100g!', 93, 0.18, 20, 20, 1.15, 'PAN'),
-(4, 'Gurke', 0.38, 2, 0, 'Gurken werden pro Stück verkauft', 16, 0.1, 3.6, 1.7, 0.7, 'NLD'),
-(5, 'Zitronen', 1.31, 100, 1, 'Zitronen werden im 500g Netzt verkauft', 29, 0.3, 9, 2.5, 1.1, 'GRC'),
-(6, 'Avocado', 1, 25, 0, 'Verkauf pro Stück\r\nVorgereift', 160, 15, 9, 0.7, 2, 'PER'),
-(7, 'Paprika bunt', 1.24, 7, 0, '500g Packung, drei Farben', 225, 22, 1.6, 0.4, 4.5, 'ESP');
+INSERT INTO `t_produkte` (`Prod_ID`, `Produkt_Name`, `Anzeige_Name`, `Kategorie`, `Nettopreis`, `Lagerbestand`, `Sales`, `Beschreibung`, `Kalorien`, `Fett`, `Kohlenhydrate`, `Zucker`, `Protein`, `Herkunft`) VALUES
+(1, 'Tomaten', 'Tomaten', 'Frucht', 0.21, 10, 0, 'Frische Rispentomaten, Stückpreis', 18, 0.2, 3.9, 2.6, 0.9, 'NLD'),
+(2, 'Trauben_hell', 'Trauben hell', 'Frucht', 1.66, 5, 1, 'Trauben hell, 250g Packung', 76, 0.4, 17, 14, 0.5, 'GRC'),
+(3, 'Banane', 'Banane', 'Frucht', 0.45, 25, 0, 'Vorgereifte Bananen, Stückpreis', 93, 0.18, 20, 20, 1.15, 'PAN'),
+(4, 'Gurke', 'Gurke', 'Obst', 0.38, 2, 0, 'Knackige Salatgurken, Stückpreis', 16, 0.1, 3.6, 1.7, 0.7, 'NLD'),
+(5, 'Zitronen', 'Zitronen', 'Frucht', 1.31, 100, 1, 'Saftige Zitronen, 500g Netz', 29, 0.3, 9, 2.5, 1.1, 'GRC'),
+(6, 'Avocado', 'Avocado', 'Exotisch', 1, 25, 0, 'Vorgereifte Hass Avocado, Stückpreis', 160, 15, 9, 0.7, 2, 'PER'),
+(7, 'Paprika_bunt', 'Bunte Paprika', 'Obst', 1.24, 7, 0, 'Frische bunte Paprika, 500g Packung', 225, 22, 1.6, 0.4, 4.5, 'ESP'),
+(8, 'Lauchzwiebeln', 'Lauchzwiebeln', 'Gemuese', 0.31, 11, 0, 'Frische Lauchzwiebeln/ Frühlingszwiebeln, Bund', 34, 0.4, 7, 2.2, 1.9, 'DEU'),
+(9, 'Suesskartoffel', 'Süßkartoffel', 'Exotisch', 0.88, 60, 1, 'Süßkartoffeln ideal für den Backofen, Stückpreis', 86, 0.1, 20, 4.2, 1.6, 'BRA'),
+(10, 'Wassermelone', 'Wassermelone', 'Obst', 5.75, 85, 1, 'Fruchtig süße Wassermelone, ca. 6kg', 30, 0.2, 8, 6, 0.6, 'GRC'),
+(11, 'Galiamelone', 'Galiamelone', 'Exotisch', 1.03, 55, 1, 'Süße Galiamelone, Stückpreis', 26, 0.1, 5.7, 4, 0.4, 'ISR'),
+(12, 'Zwiebeln', 'Gemüsezwiebeln', 'Gemuese', 1.11, 22, 0, 'Gemüsezwiebeln, 1kg Netz', 40, 0.1, 9, 4.2, 1.1, 'DEU'),
+(13, 'Knoblauch', 'Knoblauch', 'Gemuese', 1.04, 7, 0, 'Knoblauch, 250g Netz', 145, 0.1, 28.4, 11.7, 6, 'ITA'),
+(14, 'Kartoffeln', 'Kartoffeln', 'Gemuese', 1.42, 12, 0, 'Kartoffeln festkochen, 2,5kg Netz', 69, 0, 14, 2, 2, 'DEU');
 
 -- --------------------------------------------------------
 
@@ -171,7 +180,7 @@ ALTER TABLE `t_kunden`
 -- AUTO_INCREMENT für Tabelle `t_produkte`
 --
 ALTER TABLE `t_produkte`
-  MODIFY `Prod_ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `Prod_ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 
 --
 -- AUTO_INCREMENT für Tabelle `t_rechnung`
