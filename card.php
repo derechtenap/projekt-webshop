9feb7f03ea541f9474bccc887172d81cf1703b16
AntonyPaok <59829071+AntonyPaok@users.noreply.github.com>
2020-08-12 21:19:59 +0200

    Präfix "artikel_" in DB eingefügt und aus card.php entfernt

--- card.php
+++ card.php
@@ -41,7 +41,7 @@
                                 Ab <?= str_replace(".",",",round($row['Nettopreis']*1.19, 2)) ?> €<sup class="text-secondary"> /Stk.</sup>
                             </h1>
                             <small class="d-block mb-5">Nur noch <?= $row['Lagerbestand'] ?>x auf Lager!</small> 
-                            <button type="button" class="btn btn-lg btn-success rounded-0" onclick="addProduct('artikel_<?= $row['Produkt_Name'] ?>')" data-select="btn_warenkorb">
+                            <button type="button" class="btn btn-lg btn-success rounded-0" onclick="addProduct('<?= $row['Produkt_Name'] ?>')" data-select="btn_warenkorb">
                                 <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-basket-fill icon-unset" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
                                     <path fill-rule="evenodd" d="M5.071 1.243a.5.5 0 0 1 .858.514L3.383 6h9.234L10.07 1.757a.5.5 0 1 1 .858-.514L13.783 6H15.5a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-.5.5H15v5a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2V9H.5a.5.5 0 0 1-.5-.5v-2A.5.5 0 0 1 .5 6h1.717L5.07 1.243zM3.5 10.5a.5.5 0 0 0-1 0v3a.5.5 0 0 0 1 0v-3zm2.5 0a.5.5 0 0 0-1 0v3a.5.5 0 0 0 1 0v-3zm2.5 0a.5.5 0 0 0-1 0v3a.5.5 0 0 0 1 0v-3zm2.5 0a.5.5 0 0 0-1 0v3a.5.5 0 0 0 1 0v-3zm2.5 0a.5.5 0 0 0-1 0v3a.5.5 0 0 0 1 0v-3z" />
                                 </svg>
@@ -51,7 +51,7 @@
                                 <button class="btn btn-outline-secondary rounded-0" type="button" id="button-addon1" data-select="decrease" onclick="decreaseSum(1);">-</button>
                                 <input type="number" class="form-control" value="1" min="0" max="1000" step="1" aria-label="Summe" aria-describedby="button-addon1" data-select="sum">
                                 <button class="btn btn-outline-secondary rounded-0" type="button" id="button-addon2" data-select="increase" onclick="increaseSum(1);">+</button>
-                                <button class="btn btn-success rounded-0" type="button" id="button-addon2" onclick="addCart('artikel_<?= $row['Produkt_Name'] ?>');">
+                                <button class="btn btn-success rounded-0" type="button" id="button-addon2" onclick="addCart('<?= $row['Produkt_Name'] ?>');">
                                     <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-cart-check icon-unset" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
                                         <path fill-rule="evenodd" d="M11.354 5.646a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708 0l-1.5-1.5a.5.5 0 1 1 .708-.708L8 8.293l2.646-2.647a.5.5 0 0 1 .708 0z" />
                                         <path fill-rule="evenodd" d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l1.313 7h8.17l1.313-7H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm7 0a1 1 0 1 0 0 2 1 1 0 0 0 0-2z" />
